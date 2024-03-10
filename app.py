a63e7baf56657d735c35d6b78ab1f28f0317c723
saikiran <saikiran.merugu74@gmail.com>
2024-03-10 16:32:03 -0400

    added port in app.py

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def add_user():
     return render_template('index.html', message='User added successfully')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0',port =5000)
